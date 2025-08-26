46a9c908f71c00ba0f974ac5e48ad4f1505b0db7
dianagoloveiko <dianagoloveiko@gmail.com>
2025-08-26 15:04:59 +0300

    delete photo from container

--- js/init.js
+++ js/init.js
@@ -14,21 +14,69 @@ window.addEventListener ('load', allScript,false);
     function createBook() {    
         homePage.style.display = 'none';
         mainPage.style.display = 'flex';
+      
        }
 
-    class Photo {                                                 //описываем класс фото
+    class Photo {                                                 //описываем классы фото, страницы и альбом
         constructor (containerID) {
             this.container = document.getElementById(containerID);
         }
         createImage (src) {
+            const imgDiv = document.createElement('div');
             const imgElem = document.createElement('img');
+            const closeSpan = document.createElement('span');
             imgElem.src = src;
             imgElem.alt = 'Ваше добавленное фото';
             imgElem.style.width = '100px';
-            imgElem.style.margin = '2px';
-            this.container.appendChild(imgElem);
+            imgDiv.className = "imgDiv";
+            closeSpan.textContent= "x"
+            closeSpan.className = 'closeSpan';
+            this.container.appendChild(imgDiv);
+            imgDiv.appendChild(imgElem);
+            imgDiv.appendChild(closeSpan);
+            closeSpan.addEventListener('click', deleteImg, false);
+           
+            
+
         }
     }
+    class Page {
+        constructor(title) {
+            this.photos = [];
+        }
+        addPhoto (photo) {
+            this.photos.push(photo);
+        }
+        removePhoto(photoUrl) {
+            this.photos = this.photos.filter(photo => photo.url !== photoUrl);
+        }   
+        getPhotos() {
+             return this.photos;
+          }
+    }
+    class Album {
+        constructor(title) {
+            this.title = title; 
+            this.pages = [];
+        } 
+        addPage(page) {
+            this.pages.push(page);
+        }
+        removePage(pageIndex) {
+            if (pageIndex >= 0 && pageIndex < this.pages.length) {
+                this.pages.splice(pageIndex, 1);
+            } else {
+            alert(`Страницы с индексом ${pageIndex} не существует.`);
+            }
+        }
+        getPages() {
+            return this.pages;
+        }
+        getPageCount() {
+            return this.pages.length;
+        }
+    }
+
 
     function addPhotoForLoad () {                                     // по вводу каждой ссылки на фото создаем новый элемент класса Photo
         const imageURL = document.getElementById('inputUrlImg').value;
@@ -41,3 +89,9 @@ window.addEventListener ('load', allScript,false);
             document.getElementById('inputUrlImg').value = '';
         }
     }
+
+        function deleteImg (eo) {
+                const clickedImg = eo.currentTarget;
+                const parent =clickedImg.parentElement; 
+                parent.remove();
+        }
