4919327c19121daaee25bf84d61a1412420ed167
dianagoloveiko <dianagoloveiko@gmail.com>
2025-09-16 10:39:16 +0300

    fetch DIANA1

--- js/init.js
+++ js/init.js
@@ -13,6 +13,10 @@ window.addEventListener ('load', allScript,false);
     const mainPage = document.getElementById('mainPage');
     
     let nameAlbum, myAlbum;
+    let photoNumber=1;
+    let photoDivNum = 1;
+    let numPage = 1;
+
     function createBook() {    
         homePage.style.display = 'none';
         mainPage.style.display = 'flex';
@@ -20,25 +24,144 @@ window.addEventListener ('load', allScript,false);
          myAlbum = new Album(nameAlbum);
        }
 
-    
+    function loadBook() {
+        const nameForLoading = prompt('Введите название фотокниги для продолжения редактирования');
+        let sp = new URLSearchParams();
+        sp.append('f', 'READ');
+        sp.append('n', `GOLOVEIKO_PHOTOALBUM_ALBUM_${nameForLoading}`);
+        fetch(ajaxHandlerScript, { method: 'post', body: sp })
+         .then( response => response.json() )
+         .then( data => { 
+            console.log(data); 
+            homePage.style.display = 'none';
+            mainPage.style.display = 'flex';
+            console.log(data.result);
+            const myAlbumData = JSON.parse(data.result);
+             myAlbum =  Album.fromJSON(myAlbumData);
+            console.log (myAlbum);
+            numPage = myAlbum.pages.length+1;
+            for (let i=0; i <myAlbum.pages.length; i++) {
+                let page = myAlbum.pages[i];
+                if (page.numPhotos == 0) {
+                    //debugger; 
+                    page.displayPage();
+                    for (let i=0; i < page.photos.length; i++) {
+                        const photo = page.photos[i];
+                        const photoDiv = document.createElement('div');
+                        photoDiv.classList.add('photo-div');
+                        photoDiv.style.position = 'absolute';
+                        photoDiv.style.left = '10%';
+                        photoDiv.style.top = '10%';
+                        photoDiv.style.width = '30%';
+                        photoDiv.dataset.photoDivNum = photo.photoDivNum;
+                        photoDivNum++;
+                        const pageDivs = document.querySelectorAll('.page-div');
+                        const pageDiv = Array.from(pageDivs).find(div => div.getAttribute('data-page-number') == page.pageNumber);
+                        pageDiv.appendChild(photoDiv);
+                        const img = document.createElement('img');
+                        img.src = photo.url;
+                        img.style.width = '100%';
+                        img.classList.add ("photo-in-div");
+                        photoDiv.appendChild(img);
+                        const buttonDelete = document.createElement('button');
+                        buttonDelete.classList.add('buttonDeletePhoto');
+                        buttonDelete.textContent = 'удалить фото';
+                        photoDiv.appendChild(buttonDelete);
+                        buttonDelete.addEventListener('click', deletePhoto)
+                        photoDiv.addEventListener ('mousedown', onMouseDown, false);  //подписываем картинку на mousedown, чтобы можно было ее таскать
+                         for (let i=1; i<=4; i++) {                          //создаем управляющие элементы
+                        const sizeElem = document.createElement('div');
+                        sizeElem.classList.add('elemResize');
+                        photoDiv.appendChild(sizeElem);
+                        sizeElem.addEventListener ('mousedown', onMouseDown, false);
+                        console.log (photoDiv.offsetWidth);
+                            console.log (sizeElem.offsetWidth);
+                        switch (i) {
+                            case 1: 
+                                sizeElem.dataset.size = 'tl';
+                                sizeElem.style.left = '0';
+                                sizeElem.style.top = '0';
+                            break;
+                            case 2:
+                                sizeElem.dataset.size = 'tr';
+                                sizeElem.style.right = '0';
+                                sizeElem.style.top = '0';
+                                break;
+                            case 3:
+                                sizeElem.dataset.size = 'br';
+                                sizeElem.style.right = '0';
+                                    sizeElem.style.bottom = '0';
+                                break;
+                            case 4:
+                                sizeElem.dataset.size = 'bl';
+                                sizeElem.style.left = '0';
+                                sizeElem.style.bottom = '0';
+                                break;
+                            }       
+                            }
+                            }
+                        }   else {
+                     page.displayPage();
+                    for (let i=0; i < page.photos.length; i++) {
+                    //debugger;
+                    const photo = page.photos[i];
+                    photo.displayPhoto();
+                }
+                }
+                if (i === myAlbum.pages.length-1) {
+                    const activePageNum = page.pageNumber;
+                    const pageDivs = document.querySelectorAll('.page-div');
+                    const newActivePage = Array.from(pageDivs).find(div => div.getAttribute('data-page-number') === `${activePageNum}`);
+                    newActivePage.classList.add('active');
+                }
+                
+            }
+        } )
+         .catch( error => { console.error(error); } );
+    }
         
     
    
 
     document.addEventListener ('mouseup', onMouseUp, false);  
 
-       let photoNumber=1;
+     
   
    class Photo {
-    constructor(photoNumber,url,description,posX,posY) {
+    constructor(photoNumber,url,description,posX,posY, photoDivNum) {
         this.url = url;
         this.description = description;
         this.photoNumber = photoNumber;
         this.posX = posX;
         this.posY = posY;
+        this.photoDivNum = photoDivNum;
     }
+      displayPhoto () {
+        const photoDivs = document.querySelectorAll('.photo-div');
+        const photoDiv = Array.from(photoDivs).find(div => div.dataset.photoDivNum == this.photoDivNum );
+        const img = document.createElement('img');
+            img.src = this.url;
+            img.style.width = '100%';
+            img.classList.add ("photo-in-div");
+            img.dataset.photoNumber = photoNumber;
+            
+             photoNumber++;
+            photoDiv.appendChild(img);
+            const buttonDelete = document.createElement('button');
+            buttonDelete.classList.add('buttonDeletePhoto');
+            buttonDelete.textContent = 'удалить фото';
+            photoDiv.appendChild(buttonDelete);
+            buttonDelete.addEventListener('click', deletePhoto)
+        const buttons = document.querySelectorAll('.buttonAddPhoto');
+        console.log (buttons);
+        const clickedButton = Array.from(buttons).filter(button => photoDiv.contains(button));
+        console.log (clickedButton);
+        clickedButton[0].style.display = 'none';  
+
+            
+      }
       static fromJSON (data) {
-        return new Photo(data.photoNumber, data.url, data.description);
+        return new Photo(data.photoNumber, data.url, data.description,data.posX, data.posY, data.photoDivNum);
       } 
    }
     class Page {
@@ -69,6 +192,8 @@ window.addEventListener ('load', allScript,false);
                 const photoDiv=document.createElement('div');
                 photoDiv.classList.add ('photo-div');
                 photoDiv.classList.add('one-photo');
+                photoDiv.dataset.photoDivNum = photoDivNum;
+                photoDivNum++;
                 const buttonAddPhoto = document.createElement('button');
                 buttonAddPhoto.classList.add('buttonAddPhoto');
                 buttonAddPhoto.textContent = 'Добавить фото';
@@ -91,6 +216,8 @@ window.addEventListener ('load', allScript,false);
                     }
                     photoDiv.classList.add('two-photo');
                     photoDiv.id = `twoPhoto${i}`;
+                    photoDiv.dataset.photoDivNum = photoDivNum;
+                    photoDivNum++;
                     const buttonAddPhoto = document.createElement('button');
                     buttonAddPhoto.classList.add('buttonAddPhoto');
                     buttonAddPhoto.textContent = 'Добавить фото';
@@ -107,6 +234,8 @@ window.addEventListener ('load', allScript,false);
                     photoDiv.classList.add ('photo-div');
                     photoDiv.classList.add('three-photo');
                     photoDiv.id = `threePhoto${i}`;
+                    photoDiv.dataset.photoDivNum = photoDivNum;
+                    photoDivNum++;
                    if (i==1) {
                         photoDiv.style.left = '10%';
                     }
@@ -132,6 +261,8 @@ window.addEventListener ('load', allScript,false);
                     photoDiv.classList.add ('photo-div');
                     photoDiv.classList.add('four-photo');
                     photoDiv.id = `fourPhoto${i}`;
+                    photoDiv.dataset.photoDivNum = photoDivNum;
+                    photoDivNum++;
                     if (i==1) {
                         photoDiv.style.left = '10%';
                         photoDiv.style.top = '10%';
@@ -171,8 +302,9 @@ window.addEventListener ('load', allScript,false);
                     pageDiv.appendChild(buttonFinishAdding);
                     pageDiv.classList.add('emptyPage');
                     pageDiv.appendChild(buttonAddPhoto);
-                    buttonAddPhoto.addEventListener('click', () => this.addPhotoAbsolute());
+                    buttonAddPhoto.addEventListener('click', () => this.addPhotoDiv());
                     buttonFinishAdding.addEventListener('click', () => this.finishAdding());
+
             }
 
         }
@@ -184,30 +316,19 @@ window.addEventListener ('load', allScript,false);
            const photoDivClicked = clickedButton.parentNode;
            const posX = photoDivClicked.offsetLeft;
            const posY = photoDivClicked.offsetTop;
-           const newPhoto = new Photo(photoNumber,photoUrl, photoDiscription,posX, posY);
+           const clickedPhotoDivNum = photoDivClicked.getAttribute('data-photo-div-num');
+            
+           const newPhoto = new Photo(photoNumber,photoUrl, photoDiscription,posX, posY, clickedPhotoDivNum);
            
                 this.photos.push(newPhoto);
-           const img = document.createElement('img');
-            img.src = newPhoto.url;
-            img.style.width = '100%';
-            img.classList.add ("photo-in-div");
-            img.dataset.photoNumber = photoNumber;
-            
-             photoNumber++;
-            photoDivClicked.appendChild(img);
-            const buttonDelete = document.createElement('button');
-            buttonDelete.classList.add('buttonDeletePhoto');
-            buttonDelete.textContent = 'удалить фото';
-            photoDivClicked.appendChild(buttonDelete);
-            buttonDelete.addEventListener('click', deletePhoto)
-
-            clickedButton.style.display = 'none';  
+          
+            newPhoto.displayPhoto();
+            clickedButton.dataset.photoDivNum = clickedPhotoDivNum;
+            //clickedButton.style.display = 'none';  
+          
         } 
-         addPhotoAbsolute (eo) {   
+         addPhotoDiv (eo) {   
             const clickedButton = event.target;                       //создаем контейнер и фотографию в нем
-            //const index = clickedButton.getAttribute('data-index');
-            //const pagesDiv = document.querySelectorAll('.emptyPage');
-            //const pageDiv = Array.from(pagesDiv).find(div => div.dataset.pageNumber === index );
             const pageDiv = clickedButton.parentNode;
             const photoUrl = prompt('Введите url фото');
             const photoDiscription = prompt ('Введите описание фото');
@@ -219,19 +340,14 @@ window.addEventListener ('load', allScript,false);
             photoDiv.style.width = '30%';
             const posX = photoDiv.offsetLeft;
             const posY = photoDiv.offsetTop;
-            const newPhoto = new Photo(photoNumber,photoUrl, photoDiscription, posX, posY);
+            photoDiv.dataset.photoDivNum = photoDivNum;
+            pageDiv.appendChild(photoDiv);
+            const newPhoto = new Photo(photoNumber,photoUrl, photoDiscription, posX, posY, photoDivNum);
             this.photos.push(newPhoto);
+            photoDivNum++;
             photoDiv.addEventListener ('mousedown', onMouseDown, false);  //подписываем картинку на mousedown, чтобы можно было ее таскать
-            const img = document.createElement('img');
-            img.src = newPhoto.url;
-            img.style.width = '100%';
-            img.classList.add ("photo-in-div") ;
-             const buttonDelete = document.createElement('button');
-            buttonDelete.classList.add('buttonDeletePhoto');
-            buttonDelete.textContent = 'удалить фото';
-            photoDiv.appendChild(buttonDelete);
-            console.log (photoDiv.offsetWidth);
-            buttonDelete.addEventListener('click', deletePhoto)
+            newPhoto.displayPhoto();
+
             for (let i=1; i<=4; i++) {                          //создаем управляющие элементы
                 const sizeElem = document.createElement('div');
                 sizeElem.classList.add('elemResize');
@@ -262,8 +378,7 @@ window.addEventListener ('load', allScript,false);
                         break;
                 }
             }
-            pageDiv.appendChild(photoDiv);
-            photoDiv.appendChild(img);
+
         }
         finishAdding () {                      //убираем кноки добавить фото с пустой страницы
             const buttonAddPhoto = document.getElementById('nullButton');
@@ -315,8 +430,7 @@ window.addEventListener ('load', allScript,false);
    
     
     
-
-   let numPage = 1;
+   
     
     function createPage (n) {
         
@@ -458,6 +572,7 @@ window.addEventListener ('load', allScript,false);
               if (container.offsetTop + container.offsetHeight > page.offsetHeight) {
                 container.style.top = page.offsetHeight - container.offsetHeight + 'px';
               }
+              
             }
             if (draggedElem.className === 'elemResize') {
                 eo.preventDefault();
@@ -501,7 +616,7 @@ window.addEventListener ('load', allScript,false);
            
             document.removeEventListener ('mousemove', onMouseMove, false);
      }
-    const ajaxHandlerScript="https://fe.it-academy.by/AjaxStringStorage2.php";  
+      const ajaxHandlerScript="https://fe.it-academy.by/AjaxStringStorage2.php";  
 
     function saveToAjax () {
         const nameForSaving = prompt('Введите имя литанскими буквами для сохранения');
@@ -515,34 +630,9 @@ window.addEventListener ('load', allScript,false);
          .catch( error => { console.error(error); } );
     }
 
-    function loadBook() {
-        const nameForLoading = prompt('Введите название фотокниги для продолжения редактирования');
-        let sp = new URLSearchParams();
-        sp.append('f', 'READ');
-        sp.append('n', `GOLOVEIKO_PHOTOALBUM_ALBUM_${nameForLoading}`);
-        fetch(ajaxHandlerScript, { method: 'post', body: sp })
-         .then( response => response.json() )
-         .then( data => { 
-            console.log(data); 
-            homePage.style.display = 'none';
-            mainPage.style.display = 'flex';
-            console.log(data.result);
-            const myAlbumData = JSON.parse(data.result);
-            const myAlbum =  Album.fromJSON(myAlbumData);
-            console.log (myAlbum);
-            for (let i=0; i <myAlbum.pages.length; i++) {
-                let page = myAlbum.pages[i];
-                page.displayPage();
-                if (i === myAlbum.pages.length-1) {
-                    const activePageNum = page.pageNumber;
-                    const pageDivs = document.querySelectorAll('.page-div');
-                    const newActivePage = Array.from(pageDivs).find(div => div.getAttribute('data-page-number') === `${activePageNum}`);
-                    newActivePage.classList.add('active');
-                }
-            }
-        } )
-         .catch( error => { console.error(error); } );
-    }
+    
+
+   
 
      /*function saveToAjax () {
         const nameForSaving = prompt('Введите имя литанскими буквами для сохранения');
